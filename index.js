55ab60f181eb8bca00d08b05b34ce593ed7aeb28
lewis wanjohi <i.johilewis@gmail.com>
2025-09-26 20:43:47 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  // grab the <p> element with id="text"
+  const textElement = document.getElementById("text");
+
+  // update its content
+  textElement.textContent = "This is really cool!";
+});
